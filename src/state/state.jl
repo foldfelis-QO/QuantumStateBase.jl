663e37b1274b47967225f7b6a3466fa10cf4cde8
JingYu Ning <foldfelis@gmail.com>
2021-07-06 16:37:56 +0800

    operator and state

--- src/state/state.jl
+++ src/state/state.jl
@@ -8,30 +8,30 @@ export
 
 include("representation.jl")
 include("basis.jl")
-# include("operator.jl")
+include("operator.jl")
 
-# ##############
-# # pure state #
-# ##############
+##############
+# pure state #
+##############
 
-# function CoherentState(α::Arg{<:Real}; dim=DIM, rep=StateVector)
-#     return displace!(VacuumState(dim=dim, rep=rep), α)
-# end
+function CoherentState(α::Arg{<:Real}; dim=DIM, rep=StateVector)
+    return displace!(VacuumState(dim=dim, rep=rep), α)
+end
 
-# function SqueezedState(ξ::Arg{<:Real}; dim=DIM, rep=StateVector)
-#     return squeeze!(VacuumState(dim=dim, rep=rep), ξ)
-# end
+function SqueezedState(ξ::Arg{<:Real}; dim=DIM, rep=StateVector)
+    return squeeze!(VacuumState(dim=dim, rep=rep), ξ)
+end
 
-# ###############
-# # mixed state #
-# ###############
+###############
+# mixed state #
+###############
 
-# bose_einstein(n::Integer, n̄::Real) = n̄^n / (1 + n̄)^(n+1)
+bose_einstein(n::Integer, n̄::Real) = n̄^n / (1 + n̄)^(n+1)
 
-# bose_einstein(n̄::Real) = n -> bose_einstein(n, n̄)
+bose_einstein(n̄::Real) = n -> bose_einstein(n, n̄)
 
-# ThermalState(n̄::Real; dim=DIM) = StateMatrix(diagm(ComplexF64.(bose_einstein(n̄).(0:dim-1))), dim)
+ThermalState(n̄::Real; dim=DIM) = StateMatrix(diagm(ComplexF64.(bose_einstein(n̄).(0:dim-1))), dim)
 
-# function SqueezedThermalState(ξ::Arg{<:Real}, n̄::Real; dim=DIM)
-#     return squeeze!(ThermalState(n̄, dim=dim), ξ)
-# end
+function SqueezedThermalState(ξ::Arg{<:Real}, n̄::Real; dim=DIM)
+    return squeeze!(ThermalState(n̄, dim=dim), ξ)
+end
--- test/state/state.jl
+++ test/state/state.jl
@@ -7,28 +7,28 @@ Base.:(≈)(s1::StateMatrix, s2::StateMatrix) = (s1.𝛒 ≈ s2.𝛒) && (s1.dim
 
 include("representation.jl")
 include("basis.jl")
-# include("operator.jl")
+include("operator.jl")
 
-# @testset "pure state" begin
-#     dim = 70
+@testset "pure state" begin
+    dim = 70
 
-#     @test CoherentState(α(2., π/4), dim=dim) == displace!(VacuumState(dim=dim), α(2., π/4))
-#     @test CoherentState(α(2., π/4), dim=dim, rep=StateMatrix) ==
-#         displace!(VacuumState(dim=dim, rep=StateMatrix), α(2., π/4))
-#     @test SqueezedState(ξ(2., π/4), dim=dim) == squeeze!(VacuumState(dim=dim), ξ(2., π/4))
-#     @test SqueezedState(ξ(2., π/4), dim=dim, rep=StateMatrix) ==
-#         squeeze!(VacuumState(dim=dim, rep=StateMatrix), ξ(2., π/4))
-# end
+    @test CoherentState(α(2., π/4), dim=dim) == displace!(VacuumState(dim=dim), α(2., π/4))
+    @test CoherentState(α(2., π/4), dim=dim, rep=StateMatrix) ==
+        displace!(VacuumState(dim=dim, rep=StateMatrix), α(2., π/4))
+    @test SqueezedState(ξ(2., π/4), dim=dim) == squeeze!(VacuumState(dim=dim), ξ(2., π/4))
+    @test SqueezedState(ξ(2., π/4), dim=dim, rep=StateMatrix) ==
+        squeeze!(VacuumState(dim=dim, rep=StateMatrix), ξ(2., π/4))
+end
 
-# @testset "mixed state" begin
-#     dim = 70
+@testset "mixed state" begin
+    dim = 70
 
-#     n̄ = 0.5
-#     n = 5
-#     @test SqState.bose_einstein(n̄)(n) == n̄^n / (1 + n̄)^(n+1)
+    n̄ = 0.5
+    n = 5
+    @test QuantumStateBase.bose_einstein(n̄)(n) == n̄^n / (1 + n̄)^(n+1)
 
-#     @test ThermalState(n̄, dim=dim) ==
-#         StateMatrix(diagm(SqState.bose_einstein(n̄).(0:dim-1)), dim)
-#     @test SqueezedThermalState(ξ(1., π/4), n̄, dim=dim) ==
-#         squeeze!(ThermalState(n̄, dim=dim), ξ(1., π/4))
-# end
+    @test ThermalState(n̄, dim=dim) ==
+        StateMatrix(diagm(QuantumStateBase.bose_einstein(n̄).(0:dim-1)), dim)
+    @test SqueezedThermalState(ξ(1., π/4), n̄, dim=dim) ==
+        squeeze!(ThermalState(n̄, dim=dim), ξ(1., π/4))
+end
