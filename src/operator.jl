49a159bc47a0af5a644055df5890c2e0f2c1fe75
meerkat321 <yrc13211@gmail.com>
2022-02-23 14:45:34 +0800

    Add docs for operator

--- src/operator.jl
+++ src/operator.jl
@@ -7,9 +7,38 @@ export
 ############
 # a† and a #
 ############
+"""
+    QuantumStateBase.Creation(T; dim::Integer)
 
+Creation operator in matrix form.
+
+``\\hat{a}^{\\dagger}``
+"""
 Creation(T; dim::Integer) = .√(diagm(-1 => T.(Base.OneTo(dim-1))))
 
+"""
+    create(v::AbstractVector{T})
+
+    create(ρ::AbstractMatrix{T})
+
+Apply creation operator on a quantum state.
+
+## Arguments
+
+* `v`: A quantum state vector.
+* `ρ`: Density matrix of a quantum state.
+
+## Examples
+
+```julia-repl
+julia> state = VacuumState(dim = 5);
+
+julia> new_state = Create(state);
+
+julia> new_state == SinglePhotonState(dim = 5)
+true
+```
+"""
 function create(v::AbstractVector{T}) where {T}
     dim = length(v)
 
@@ -23,8 +52,38 @@ function create(ρ::AbstractMatrix{T}) where {T}
     return c * ρ * c'
 end
 
+"""
+    QuantumStateBase.Annihilation(T; dim::Integer)
+
+Annihilation operator in matrix form.
+
+``\\hat{a}``
+"""
 Annihilation(T; dim::Integer) = .√(diagm(1 => T.(Base.OneTo(dim-1))))
 
+"""
+    annihilate(v::AbstractVector{T})
+
+    annihilate(ρ::AbstractMatrix{T})
+
+Apply annihilation operator on a quantum state.
+
+## Arguments
+
+* `v`: A quantum state vector.
+* `ρ`: Density matrix of a quantum state.
+
+## Examples
+
+```julia-repl
+julia> state = SinglePhotonState(dim = 5);
+
+julia> new_state = annihilate(state);
+
+julia> new_state == VacuumState(dim = 5)
+true
+```
+"""
 function annihilate(v::AbstractVector{T}) where {T}
     dim = length(v)
 
@@ -47,18 +106,41 @@ struct ComplexVec{T<:Real}
     θ::T
 end
 
+"""
+    QuantumStateBase.ComplexVec(r::T, θ::T)
+
+Vector in polar coordinate for complex plane.
+
+``v = re^{-i\\theta}``
+
+## Arguments
+
+* `r`: Radius of the complex number.
+* `θ`: Argument of the complex number.
+"""
 ComplexVec(r::T, θ::T) where {T} = ComplexVec{T}(r, θ)
 
 function Base.show(io::IO, complexvec::ComplexVec{T}) where {T}
     print(io, "ComplexVec{$T}($(complexvec.r)exp(-$(complexvec.θ)im))")
 end
 
+"""
+    QuantumStateBase.z(complexvec::ComplexVec)
+
+Evaluate the complex vector.
+"""
 z(complexvec::ComplexVec) = complexvec.r * exp(-im * complexvec.θ)
 
 ################
 # displacement #
 ################
+"""
+    QuantumStateBase.Displacement(T::Type{<:Complex}, r::Real, θ::Real; dim)
 
+Displacement operator in matrix form.
+
+``\\hat{D}(\\alpha) = exp(\\alpha \\hat{a}^{\\dagger} - \\alpha^{*} \\hat{a})``
+"""
 function Displacement(T::Type{<:Complex}, r::Real, θ::Real; dim)
     U = real(T)
     α = ComplexVec(U(r), U(θ))
@@ -68,6 +150,31 @@ function Displacement(T::Type{<:Complex}, r::Real, θ::Real; dim)
     )
 end
 
+"""
+    displace(v::AbstractVector{T}, r, θ)
+
+    displace(ρ::AbstractMatrix{T}, r, θ)
+
+Apply displacement operator on a quantum state.
+
+## Arguments
+
+* `v`: A quantum state vector.
+* `ρ`: Density matrix of a quantum state.
+* `r`: Radius of displacement.
+* `θ`: Phase of displacement.
+
+## Examples
+
+```julia-repl
+julia> state = VacuumState(dim = 5);
+
+julia> new_state = displace(state, 2, π/4);
+
+julia> new_state == CoherentState(2, π/4, dim = 5)
+true
+```
+"""
 function displace(v::AbstractVector{T}, r, θ) where {T}
     dim = length(v)
 
@@ -85,6 +192,18 @@ end
 # squeezing #
 #############
 
+"""
+    QuantumStateBase.Squeezing(T::Type{<:Complex}, r::Real, θ::Real; dim)
+
+Squeezing operator in matrix form.
+
+## Arguments
+
+* `r`: Squeezing level.
+* `θ`: Squeezing phase.
+
+``\\hat{S}(\\xi) = exp(\\frac{1}{2}(\\xi^{*} \\hat{a}^{2} - \\xi \\hat{a}^{\\dagger 2}))``
+"""
 function Squeezing(T::Type{<:Complex}, r::Real, θ::Real; dim)
     U = real(T)
     ξ = ComplexVec(U(r), U(θ))
@@ -94,6 +213,31 @@ function Squeezing(T::Type{<:Complex}, r::Real, θ::Real; dim)
     )
 end
 
+"""
+    squeeze(v::AbstractVector{T})
+
+    squeeze(ρ::AbstractMatrix{T})
+
+Apply squeezing operator on a quantum state.
+
+## Arguments
+
+* `v`: A quantum state vector.
+* `ρ`: Density matrix of a quantum state.
+* `r`: Squeezing level.
+* `θ`: Squeezing phase.
+
+## Examples
+
+```julia-repl
+julia> state = VacuumState(dim = 5);
+
+julia> new_state = squeeze(state, 0.5, π/4);
+
+julia> new_state == SqueezedState(0.5, π/4, dim = 5)
+true
+```
+"""
 function squeeze(v::AbstractVector{T}, r, θ) where {T}
     dim = length(v)
 
